8212114dc75031b60be6ebf9e14eab22c0909cc4
JameshaRicketts <jamesharicketts@gmail.com>
2023-02-08 00:41:53 +0000

    List has a lot of methods

--- src/main/java/ListExercise.java
+++ src/main/java/ListExercise.java
@@ -30,7 +30,7 @@ public class ListExercise {
      * @return a List<Integer> object.
      */
     public List<Integer> createList(){
-        return null;
+        return new ArrayList<Integer>();
     }
 
     /**
@@ -40,7 +40,7 @@ public class ListExercise {
      * @return the size of List (number of items it holds.)
      */
     public int getSize(List<Integer> list){
-        return 0;
+        return list.size();
     }
 
     /**
@@ -52,6 +52,7 @@ public class ListExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addToList(List<Integer> list, int value){
+        list.add(value);
     }
 
     /**
@@ -67,7 +68,8 @@ public class ListExercise {
      * @return the int at the location in 'list' represented by 'index'.
      */
     public int get(List<Integer> list, int index){
-        return 0;
+        //.get() returs the elemnt, .indexOf() returns the index of the first occurrence of the element 
+        return list.get(index);
     }
 
     /**
@@ -80,7 +82,10 @@ public class ListExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeFromList(List<Integer> list, int position){
+        list.remove(position);
 
+        //I believe this would also be valid, but I think it's slightly redundant
+        //list.remove(list.get(position));
     }
 
     /**
@@ -93,5 +98,7 @@ public class ListExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void updateAtPosition(List<Integer> list, int position, int value){
+        //list.remove();
+        list.set(position, value);
     }
 }
